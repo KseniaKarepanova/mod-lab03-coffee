e06e4a43f1bfae970a6dcf489936eee0a0b4a0e8
KseniaKarepanova <126484812+KseniaKarepanova@users.noreply.github.com>
2023-03-17 21:04:22 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include "Automat.h"
+using namespace std;
+
+int main()
+{
+    string menu[] = { "Latte", "Espresso macchiato", "Americano ", "Cappuccino", "Coffee with milk"};
+    int prices[] = {100, 110, 80, 110,70};
+    
+    Automat automat(menu, prices, 5);
+    automat.on();
+    cout << automat.getState()<<endl;
+    automat.coin(100);
+    cout << automat.getState() << endl;
+    automat.cancel();
+    cout << automat.getState() << endl;
+    automat.coin(100);
+    cout << automat.getState() << endl;
+
+    automat.getMenu();
+    automat.choice(1);
+    cout << automat.getState() << endl;
+
+    automat.check();
+    cout << automat.getState() << endl;
+   
+    automat.coin(100);
+    cout << automat.getState() << endl;
+    automat.choice(1);
+    cout << automat.getState() << endl;
+
+    automat.check();
+    cout << automat.getState() << endl;
+    automat.cook();
+    cout << automat.getState() << endl;
+    automat.finish();
+    cout << automat.getState() << endl;
+}
