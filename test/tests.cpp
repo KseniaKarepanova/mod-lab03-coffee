130b93f8e9cb4247909358f3423c4ebb81434654
KseniaKarepanova <126484812+KseniaKarepanova@users.noreply.github.com>
2023-03-18 00:05:36 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,67 +3,67 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
 TEST(test1, test_wait) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
-  string expected_state = "Off";
+  std::string expected_state = "Off";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test2, test_wait) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   automat.on();
-  string expected_state = "Wait";
+  std::string expected_state = "Wait";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test3, test_accept) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   automat.on();
   automat.coin(100);
-  string expected_state = "Accept";
+  std::string expected_state = "Accept";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test4, test_accept) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   automat.on();
   automat.coin(100);
   automat.cancel();
-  string expected_state = "Wait";
+  std::string expected_state = "Wait";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test5, test_accept) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   automat.on();
   automat.coin(100);
   automat.choice(1);
-  string expected_state = "Check";
+  std::string expected_state = "Check";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test6, test_check) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   automat.on();
   automat.coin(100);
   automat.choice(1);
-  string expected_state = "Check";
+  std::string expected_state = "Check";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test7, test_check) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   automat.on();
@@ -71,12 +71,12 @@ TEST(test7, test_check) {
   automat.coin(150);
   automat.choice(1);
   automat.check();
-  string expected_state = "Cook";
+  std::string expected_state = "Cook";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test8, test_check) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   Automata automat(menu, prices, 5);
@@ -84,24 +84,24 @@ TEST(test8, test_check) {
   automat.coin(100);
   automat.choice(1);
   automat.check();
-  string expected_state = "Accept";
+  std::string expected_state = "Accept";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test9, test_check) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   automat.on();
   automat.coin(100);
   automat.choice(1);
   automat.cancel();
-  string expected_state = "Wait";
+  std::string expected_state = "Wait";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test10, test_cook) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   automat.on();
@@ -110,12 +110,12 @@ TEST(test10, test_cook) {
   automat.choice(1);
   automat.check();
   automat.cook();
-  string expected_state = "Cook";
+  std::string expected_state = "Cook";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
 TEST(test11, test_cook) {
-  string menu[] = { "Latte", "Espresso macchiato", "Americano"};
+  std::string menu[] = { "Latte", "Espresso macchiato", "Americano"};
   int prices[] = {100, 110, 80};
   Automata automat(menu, prices, 3);
   automat.on();
@@ -125,7 +125,7 @@ TEST(test11, test_cook) {
   automat.check();
   automat.cook();
   automat.finish();
-  string expected_state = "Wait";
+  std::string expected_state = "Wait";
   EXPECT_EQ(automat.getState(), expected_state);
 }
 
