98267f0bc6db2f116d617a9b1cabf6a88c8cf783
KseniaKarepanova <126484812+KseniaKarepanova@users.noreply.github.com>
2023-03-17 22:16:14 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,27 +1,26 @@
+#define INCLUDE_AUTOMATA_H_
 #include<iostream>
 #include <string>
-using namespace std;
 
-class Automata
-{
+class Automata {
 private:
-	enum States { Off, Wait, Accept, Check, Cook }; //перечисляем состояния автомата
-	int cash; //хранение текущей суммы
-	string* menu; //меню
-	int* prices;//цены
-	States state;//состояние текущее
-	int chosen; //выбранный напиток
-	int count; //количество напитков в меню
+    enum States { Off, Wait, Accept, Check, Cook }; //перечисляем состояния автомата
+    int cash; //хранение текущей суммы
+    std::string* menu; //меню
+    int* prices;//цены
+    States state;//состояние текущее
+    int chosen; //выбранный напиток
+    int count; //количество напитков в меню
 public:
-	Automata(string menu[], int prices[], int count);
-	void on();
-	void off();
-	void coin(int summa);
-	void getMenu();
-	string getState();
-	void choice(int chosen);
-	void check();
-	void cancel();
-	void cook();
-	void finish();
+    Automata(std::string menu[], int prices[], int count);
+    void on();
+    void off();
+    void coin(int summa);
+    void getMenu();
+    std::string getState();
+    void choice(int chosen);
+    void check();
+    void cancel();
+    void cook();
+    void finish();
 };
